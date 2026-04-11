b4e5495cdd424a2cda4edf62b5872b771f2bde28
SHU-red <68030498+SHU-red@users.noreply.github.com>
2026-04-11 23:29:40 +0200

    feat: Extract styles to dedicated CSS file
    Moves the inline CSS from `configure.phtml` into a new `static/style.css` file and registers it via the extension. This improves maintainability and separation of concerns.

--- extension.php
+++ extension.php
@@ -5,6 +5,7 @@ class EinkPush2Extension extends Minz_Extension {
     public function init() {
         $this->registerController('einkpush2');
         $this->registerTranslates();
+        $this->registerCss('style.css');
     }
 
     public function handleConfigureAction() {

a7d470bf081a1c6e48e3cab5d570d7d9b7f6b82c
SHU-red <schuhmann.sebastian@gmail.com>
2026-04-27 22:54:12 +0200

    fix: sidebar button border color match background

--- static/script.js
+++ static/script.js
@@ -610,20 +610,20 @@
             let addBtn = document.querySelector('#btn-add');
             let stick = document.querySelector('.stick.configure-feeds');
 
-            let btnWidth, btnBorder;
-            if (stick) {
-                let cs = window.getComputedStyle(stick);
-                btnWidth = cs.width;
-                btnBorder = cs.border;
-            } else if (subBtn) {
+            let btnWidth, btnBorderRadius;
+            if (subBtn) {
                 let cs = window.getComputedStyle(subBtn);
+                btnBorderRadius = cs.borderRadius;
                 let subW = parseFloat(cs.width);
                 let addW = addBtn ? parseFloat(window.getComputedStyle(addBtn).width) : 36;
                 btnWidth = (subW + addW) + 'px';
-                btnBorder = cs.border;
+            } else if (stick) {
+                let cs = window.getComputedStyle(stick);
+                btnBorderRadius = cs.borderRadius;
+                btnWidth = cs.width;
             } else {
                 btnWidth = '195px';
-                btnBorder = '1px solid #333';
+                btnBorderRadius = '3px';
             }
 
             const wrapper = document.createElement('div');
@@ -637,9 +637,9 @@
             btn.style.width = btnWidth;
             btn.style.minHeight = '25px';
             btn.style.boxSizing = 'border-box';
-            btn.style.borderRadius = '2px';
+            btn.style.borderRadius = btnBorderRadius;
             btn.style.background = '#e66a19';
-            btn.style.border = btnBorder || '1px solid #e66a19';
+            btn.style.border = '1px solid #e66a19';
             btn.style.color = '#fff';
             btn.style.cursor = 'pointer';
             btn.style.fontFamily = '"OpenSans", Cantarell, Helvetica, Arial, sans-serif';

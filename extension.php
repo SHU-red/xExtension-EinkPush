b59d3ccfd07bd1a51875166fe5ba0d4f6675f255
SHU-red <schuhmann.sebastian@gmail.com>
2026-04-28 20:10:38 +0200

    chore: v1.2.0 release (README, changelog, version bump)

--- extension.php
+++ extension.php
@@ -1,7 +1,7 @@
 <?php
 
 class EinkPushExtension extends Minz_Extension {
-    const VERSION = '1.1.9';
+    const VERSION = '1.2.0';
 
     public function init() {
         $this->registerController('EinkPush');
